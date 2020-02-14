29435ecadb9958561806aa2383b1bbf1a50f2a14
Lela Smith <smith.lelaa@gmail.com>
2020-02-14 10:01:22 -0800

    setup file structure for project

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import Navbar from './components/Navbar';
+import { Switch, Route } from 'react-router-dom';
+
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-
+        <Navbar />
+        <Header />
+        <Switch>
+        <Route exact path='/' component={} />
+        </Switch>
+        <Footer />
     </div>
   );
 }
